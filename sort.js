22fdf581c3df4c32c679d1f555b700167dd59700
Matt <matt.latimer@gmail.com>
2017-09-07 19:36:34 +0000

    added custom sorting function

--- sort.js
+++ sort.js
@@ -4,3 +4,15 @@ var students = [
   { id: 3, name: "alex",     age: 22 },
   { id: 4, name: "alex",     age: 30 }
 ];
+
+students.sort(function (a, b) {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return b.age - a.age;
+});
+
+console.log(students);
